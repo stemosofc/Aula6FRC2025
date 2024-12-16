1e5ce364512fe4dad1e6b649715101a312559dbf
Carvalho <lucacarvalhojeo@gmail.com>
2024-12-16 11:31:38 -0300

    fix: não utilizando drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,16 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -23,8 +29,14 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  // Objeto de motor Spark Max
-  SparkMax motor = new SparkMax(0, null);
+  // Objeto que será utilizado para digirir nossa tração
+  DifferentialDrive chassi;
+
+  // Aqui declaramos nossos objetos de motores SparkMax
+  public SparkMax motorEsquerdaMestre = new SparkMax(1, MotorType.kBrushless);
+  public SparkMax motorEsquerda = new SparkMax(2, MotorType.kBrushless);
+  public SparkMax motorDireitaMestre = new SparkMax(3, MotorType.kBrushless);
+  public SparkMax motorDireita = new SparkMax(4, MotorType.kBrushless);
 
   // Objeto que conta o tempo decorrido
   Timer timer = new Timer();
@@ -34,7 +46,51 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
   @Override
-  public void robotInit() {
+  public void robotInit() {    
+    /*
+    * Crie novos objetos de configuração para os SPARK MAX. 
+    * Eles irão armazenar os parâmetros de configuração 
+    * para os SPARK MAX que definiremos abaixo.
+    */
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig ConfigDireitaMestre = new SparkMaxConfig();
+    SparkMaxConfig ConfigEsquerda = new SparkMaxConfig();
+    SparkMaxConfig ConfigDireita = new SparkMaxConfig();
+    /*
+     * Coloque os parametros que vão ser aplicados para todos os
+     * SparkMAXs, também será utilizado para a configuração do Esquerda Mestre
+     */
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
+        // Aplique a configuração global e inverta, já que está no lado oposto
+    ConfigDireitaMestre
+        .apply(globalConfig)
+        .inverted(true);
+    // Aplique a configuração global e configure o SPARK líder para o modo seguidor
+    ConfigEsquerda
+        .apply(globalConfig)
+        .follow(motorEsquerdaMestre);
+    // Aplique a configuração global e configure o SPARK líder para o modo seguidor
+    ConfigDireita
+        .apply(globalConfig)
+        .follow(motorDireitaMestre);
+
+    /*
+    * Aplique a configuração aos SPARKs.
+    *
+    * kResetSafeParameters é usado para colocar o SPARK MAX em um estado conhecido. 
+    * Isso é útil caso o SPARK MAX precise ser substituído.
+    *
+    * kPersistParameters é usado para garantir que a configuração não seja perdida 
+    * quando o SPARK MAX perde energia. Isso é útil para ciclos de energia que podem 
+    * ocorrer durante a operação.
+    */
+    motorEsquerdaMestre.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorEsquerda.configure(ConfigEsquerda, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorDireitaMestre.configure(ConfigDireitaMestre, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorDireita.configure(ConfigDireita, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
@@ -63,7 +119,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
     timer.start();
   }
@@ -73,32 +128,34 @@ public class Robot extends TimedRobot {
   int contador = 0;
   @Override
   public void autonomousPeriodic() {
+    double tempo = timer.get();
     switch (m_autoSelected) {
       case kCustomAuto:
         break;
       case kDefaultAuto:
-        // Retornamos o valor do tempo
-        double tempo = timer.get();
         // Seguimos a mesma lógica para fazermos vários movimentos
         if(contador == 0) {
-          acionaMotor(20, tempo, 0.4);
+          acionaChassi(20, tempo, 0.5, 0);
         }
         if(contador == 1) {
-          acionaMotor(40, tempo, 0.5);
+          acionaChassi(30, tempo, 0.3, 1);
+        }
+        if(contador == 2) {
+          acionaChassi(20, tempo, 1, 0);
         }
         break;
     }
   }
 
+
   // Função criada para simplificar o código de acionamento dos motores
-  private void acionaMotor(double setPoint, double variavelProcesso, double speed) {
+  private void acionaChassi(double setPoint, double variavelProcesso, double speed, double rotacao) {
     if(variavelProcesso <= setPoint) {
-      motor.set(speed);
-    } else if(variavelProcesso > setPoint) {
-      motor.set(-speed);
-    } else {
-      motor.set(0);
+      chassi.arcadeDrive(speed, rotacao);
+    }else {
+      chassi.arcadeDrive(0, 0);
       contador++;
+      timer.reset();
     }
   }
 
